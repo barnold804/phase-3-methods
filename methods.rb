9698b41be8bf1de8af55aa7f20b5af408b6c59a8
Brian Arnold <b.arnold09@gmail.com>
2022-03-14 13:36:48 -0400

    commit tree 9596c9e8a3597ca81eee8655cc81f23b839456b3

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+greet "Jimmy"
+
+def greet_with_default(name = "Naureen")
+    puts "Hello, programmer!"
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(number)
+    if number.is_a?(Integer)
+    number / 2
+    else return nil
+    end
+end
+
